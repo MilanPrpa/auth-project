f8cbc0fe1b84f52681b07c111d25eb2845ef1e92
MilanPrpa <milanprpa25@gmail.com>
2020-12-28 17:39:24 -0700

    added ignore files

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+
+
+
